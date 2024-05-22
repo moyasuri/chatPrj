1aee016dcd0c1e628e1101686dffc5e88f1b23e2
moyasuri <moyasuri.hana@gmail.com>
2024-05-22 16:08:02 +0900

    first commit

---
+++ Client/MyForm.cpp
@@ -0,0 +1,30 @@
+﻿#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace Client;
+
+
+
+
+
+
+
+[STAThread]
+
+
+void main(array<String^>^ args) {
+
+	
+
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	MyForm my;
+	Application::Run(% my);
+
+
+
+
+	
+
+}
